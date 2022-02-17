6f3f4a1faf72943db25c0235c86a739902736a66
Loures1 <lucasantonio.loures@gmail.com>
2022-02-17 12:36:48 -0300

    Fncionado o grafico da cpu

--- Tela.py
+++ Tela.py
@@ -1,15 +1,18 @@
 import PySimpleGUI as sg
 import Monitoramento
 
-GRAPH_SIZE = (120, 54)
-DATA_SIZE = (120, 54)
-STEP_SIZE = 2
+GRAPH_SIZE_CPU_AND_MEMORIA = (120, 54)
+GRAPH_SIZE_MAIN = (GRAPH_SIZE_CPU_AND_MEMORIA[0]*5, GRAPH_SIZE_CPU_AND_MEMORIA[1]*5)
+DATA_SIZE_CPU_AND_MEMORIA = (120, 54)
+DATA_SIZE_MAIN = (DATA_SIZE_CPU_AND_MEMORIA[0]*5, DATA_SIZE_CPU_AND_MEMORIA[1]*5)
+STEP_SIZE_CPU_MEMORIA = 2
+STEP_SIZE_MAIN = STEP_SIZE_CPU_MEMORIA*5
 DELAY = 1000
     
 
-cpu_graph = [sg.Graph(GRAPH_SIZE, (0, 0), DATA_SIZE, background_color = 'white', key = 'cpu_graph')]
+cpu_graph = [sg.Graph(GRAPH_SIZE_CPU_AND_MEMORIA, (0, 0), DATA_SIZE_CPU_AND_MEMORIA, background_color = 'white', key = 'cpu_graph')]
 botao_cpu = [sg.Button('CPU')]
-memoria_graph = [sg.Graph(GRAPH_SIZE, (0, 0), DATA_SIZE, background_color = 'white', key = 'memoria_graph')]
+memoria_graph = [sg.Graph(GRAPH_SIZE_CPU_AND_MEMORIA, (0, 0), DATA_SIZE_CPU_AND_MEMORIA, background_color = 'white', key = 'memoria_graph')]
 botao_memoria = [sg.Button('Memória')]
 
 coluna1 =  [
@@ -20,7 +23,7 @@ coluna1 =  [
 coluna2 =   [       
             [sg.Text('', key = 'titulo')],                                                                                                                     
             [sg.Text('%utilização                                                                                                                                   100%')],
-            [sg.Frame('', [[sg.Graph((GRAPH_SIZE[0]*5, GRAPH_SIZE[1]*5), (0, 0), (DATA_SIZE[0]*5, DATA_SIZE[1]*5), key = 'graph')]])],
+            [sg.Frame('', [[sg.Graph(GRAPH_SIZE_MAIN, (0, 0), DATA_SIZE_MAIN, key = 'graph')]])],
             [sg.Text('60 segudos                                                                                                                                       0')]
 ]          
 layout =    [
@@ -29,26 +32,43 @@ layout =    [
 
 window = sg.Window('Monitoramento', layout)
 
-step_size = STEP_SIZE
-graph_cpu = False
-graph_memoria = False
-auto = 0
-lasty = 0
+auto = False
+lasty = [0]
+
 
 while True:
     event, values = window.read(timeout = DELAY, timeout_key= 1)
 
     if event == sg.WIN_CLOSED:
         break
-    
-    if type(event) == int:
-        auto += event
 
-    x, y = GRAPH_SIZE[0], (Monitoramento.CPU()*GRAPH_SIZE[1])/100
-    lastx = GRAPH_SIZE[0] - step_size
-    window['cpu_graph'].Move(-step_size, 0)
-    window['cpu_graph'].draw_line((lastx, lasty), (x, y), width=1)
-    lasty= y
+    x, y = GRAPH_SIZE_CPU_AND_MEMORIA[0], (Monitoramento.CPU()*GRAPH_SIZE_CPU_AND_MEMORIA[1])/100
+    lastx = GRAPH_SIZE_CPU_AND_MEMORIA[0] - STEP_SIZE_CPU_MEMORIA
+    window['cpu_graph'].Move(-STEP_SIZE_CPU_MEMORIA, 0)
+    window['cpu_graph'].draw_line((lastx, lasty[-1]), (x, y), width=1)
+    lasty.append(y)
+
+    if event == 'CPU' or auto == True :
+
+        if auto == True:
+            x, y = GRAPH_SIZE_MAIN[0], y*5
+            lastx = GRAPH_SIZE_MAIN[0] - STEP_SIZE_MAIN
+            window['graph'].Move(-STEP_SIZE_MAIN, 0)
+            window['graph'].draw_line((lastx, lasty[-2]*5), (x, y), width=1)
+        
+        
+        if not auto:
+            i = 0
+            for y in lasty[1::]:
+                x, y = GRAPH_SIZE_MAIN[0], y*5
+                lastx = GRAPH_SIZE_MAIN[0] - STEP_SIZE_MAIN
+                window['graph'].Move(-STEP_SIZE_MAIN, 0)
+                window['graph'].draw_line((lastx, lasty[i]*5), (x, y), width=1)
+                i += 1
+            
+            auto = True
+
+        
 
 
 window.close()
